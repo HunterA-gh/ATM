d5d804013f0900351cb91895e8d2c79d482c6e14
CaesarKCooper <caesarkasambacooperjr@gmail.com>
2021-04-13 17:56:55 -0400

    here we go again again

---
+++ src/main/java/AccountTypes.java
@@ -0,0 +1,22 @@
+public abstract class AccountTypes {
+    private double inAccount;
+
+    public double withdraw(double amount){
+        inAccount -= amount;
+        return inAccount;
+    }
+
+    public double deposit(double amount){
+        inAccount += amount;
+        return inAccount;
+    }
+
+    public double getInAccount() {
+        return inAccount;
+    }
+
+    public void giveTransfer(String account, double amount){
+    }
+
+    //public void rec
+}
---
+++ src/main/java/Investment.java
@@ -0,0 +1,20 @@
+public class Investment extends AccountTypes{
+    private double inAccount;
+
+    @Override
+    public double withdraw(double amount){
+        inAccount -= amount;
+        return inAccount;
+    }
+
+    @Override
+    public double deposit(double amount){
+        inAccount += amount;
+        return inAccount;
+    }
+
+    @Override
+    public double getInAccount() {
+        return inAccount;
+    }
+}
