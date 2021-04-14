83d19791ce3d8038faffe47d103db7095afc8286
Jacenter <JNovack1501@gmail.com>
2021-04-14 11:42:05 -0400

    working on menu after creating new account

--- src/main/java/Console.java
+++ src/main/java/Console.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 
 public class Console {
+    static Scanner atmScanner = new Scanner(System.in);
     public static void main(String[] args) {
 
 
@@ -15,13 +16,13 @@ public class Console {
         System.out.printf("%5s-%5s\n", "[1] ", " Log in to existing account.");
         System.out.printf("%5s-%5s\n", "[2] ", " Create a new account.");
         System.out.print(": ");
-        Scanner atmScanner = new Scanner(System.in);
         int inputOnHomeScreen = atmScanner.nextInt();
         if (inputOnHomeScreen == 1) {
             Accounts.logIntoExistingAccount();
         }
         if (inputOnHomeScreen == 2) {
             Accounts.createNewAccount();
+            afterCreateNewAccountScreen();
 
         }   
     }
@@ -30,15 +31,31 @@ public class Console {
         // after creating a new account on the bootUpATM
         // this will be the menu the user can interact with
         System.out.println("Welcome to your new account." + "\n What would you like to do?");
+        System.out.printf("%5s-%5s\n", "[1] ", " Create a new Checking account.");
+        System.out.printf("%5s-%5s\n", "[2] ", " Create a new Savings account.");
+        System.out.printf("%5s-%5s\n", "[3] ", " Create a new Investment account.");
+        int inputOnNewAccountUserScreen = atmScanner.nextInt();
+        switch (inputOnNewAccountUserScreen) {
+            case 1: {
+                //openNewCheckingAccount()
+                break;
+            }
+            case 2: {
+                //createnewsavingsaccount();
+                break;
+            }
+            case 3: {
+                //createnewinvetsmentaccount();
+                break;
+            }
+        }
 
-
-    }
+        }
 
     public static void afterLogIntoExistingAccountScreen() {
         // after logging into an existing account
         // this will be the menu the user can interact with
     }
-}
 
     public static void bootUpLogo() {
         System.out.println("\n" +
