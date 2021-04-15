0aeb541c82cb9b0f34020391e06784083c0ac662
Hunter <79298881+IsThisUsernameTaken1@users.noreply.github.com>
2021-04-15 14:10:02 -0400

    removed comments

--- src/main/java/Savings.java
+++ src/main/java/Savings.java
@@ -4,16 +4,5 @@ public class Savings extends AccountTypes {
         super(balance);
     }
 
-    /*public void giveTransfer(int account, double amount){ //OLD METHOD
-        if (account == 1 && getInAccount() >= amount){
-            //Get Checking
-        }else if (account == 3 && getInAccount() >= amount){
-            //Get Investment
-        }else{
-            System.out.println("Invalid account");
-        }
-        System.out.println(amount + " hase been transferred to " + account);
-    }*/
-
 }
 
