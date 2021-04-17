3d698658ea9b8d4c6904df0415cd2665aa900d1f
jonyboi21 <jonyboi21@github.com>
2021-04-17 12:12:39 -0400

    Fixed bugs that kicks you out on invalid input

--- src/main/java/Console.java
+++ src/main/java/Console.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
@@ -18,8 +17,8 @@ public class Console {
         System.out.printf("%5s-%5s\n", "[1] ", " Log in to existing account.");
         System.out.printf("%5s-%5s\n", "[2] ", " Create a new account.");
         System.out.print(": ");
-        int inputOnHomeScreen = atmScanner.nextInt();
 
+        int inputOnHomeScreen = atmScanner.nextInt();
         if (inputOnHomeScreen == 1) {
             UserFunctions.logIntoExistingUserProfile();
         }
@@ -27,15 +26,7 @@ public class Console {
             UserFunctions.createNewUserProfile();
             afterCreateNewUserProfileResetScreen();
         }
-
-
-
     }
-
-
-
-
-
 
     public static void afterCreateNewUserProfileResetScreen() {
         bootUpATM();
@@ -76,7 +67,6 @@ public class Console {
                 checkingAccountMenu();
                 break;
             }
-
             case 5: {
                 savingsAccountMenu();
                 break;
@@ -131,59 +121,65 @@ public class Console {
         System.out.println("Please enter your password : ");
         Scanner scanner = new Scanner(System.in);
         String thisPassword = scanner.nextLine();
-        for (int i = 0; i < UsersArrayList.size(); i++) {
-            if (UsersArrayList.get(i).getPassword().equals(thisPassword)) {
-                System.out.println("Welcome to your Checking account: " + "What do you want to do?");
-                System.out.printf("%5s-%5s\n", "[1] ", "Withdraw");
-                System.out.printf("%5s-%5s\n", "[2] ", "Deposit");
-                System.out.printf("%5s-%5s\n", "[3] ", "Transfer");
-                System.out.printf("%5s-%5s\n", "[4] ", "Print Transaction History");
-                System.out.printf("%5s-%5s\n", "[5] ", "Return to Previous Screen");
-                System.out.printf("%5s-%5s\n", "[6] ", "Return to Main Menu");
-                System.out.printf("%5s-%5s\n", "[7] ", "Close this account");
-                System.out.print(": ");
-                int inputOnCheckingAccountMenu = atmScanner.nextInt();
-                switch (inputOnCheckingAccountMenu) {
-                    case 1: {
-                        System.out.println("Enter amount to withdraw:");
-                        UsersArrayList.get(i).getChecking().withdraw(atmScanner.nextDouble());
-                        UsersArrayList.get(i).getChecking().printBalance();
-                        checkingAccountMenu();
-                        break;
-                    }
-                    case 2: {
-                        System.out.println("Enter amount to deposit:");
-                        UsersArrayList.get(i).getChecking().deposit(atmScanner.nextDouble());
-                        UsersArrayList.get(i).getChecking().printBalance();
-                        checkingAccountMenu();
-                        break;
-                    }
-                    case 3: {
-                        UsersArrayList.get(i).transferFromChecking();
-                        checkingAccountMenu();
-                        break;
-                    }
-                    case 4: {
-                        UsersArrayList.get(i).getChecking().getTransactionHistory();
-                        checkingAccountMenu();
-                        break;
-                    }
-                    case 5: {
-                        afterLogIntoExistingUserProfile();
-                        break;
-                    }
-                    case 6: {
-                        bootUpATM();
-                        break;
-                    }
-                    case 7: {
-                        UserFunctions.deleteCheckingAccount();
-                        afterLogIntoExistingUserProfile();
+        try {
+            for (int i = 0; i < UsersArrayList.size(); i++) {
+                if (UsersArrayList.get(i).getPassword().equals(thisPassword)) {
+                    System.out.println("Welcome to your Checking account: " + "What do you want to do?");
+                    System.out.printf("%5s-%5s\n", "[1] ", "Withdraw");
+                    System.out.printf("%5s-%5s\n", "[2] ", "Deposit");
+                    System.out.printf("%5s-%5s\n", "[3] ", "Transfer");
+                    System.out.printf("%5s-%5s\n", "[4] ", "Print Transaction History");
+                    System.out.printf("%5s-%5s\n", "[5] ", "Return to Previous Screen");
+                    System.out.printf("%5s-%5s\n", "[6] ", "Return to Main Menu");
+                    System.out.printf("%5s-%5s\n", "[7] ", "Close this account");
+                    System.out.print(": ");
+                    int inputOnCheckingAccountMenu = atmScanner.nextInt();
+                    switch (inputOnCheckingAccountMenu) {
+                        case 1: {
+                            System.out.println("Enter amount to withdraw:");
+                            UsersArrayList.get(i).getChecking().withdraw(atmScanner.nextDouble());
+                            UsersArrayList.get(i).getChecking().printBalance();
+                            checkingAccountMenu();
+                            break;
+                        }
+                        case 2: {
+                            System.out.println("Enter amount to deposit:");
+                            UsersArrayList.get(i).getChecking().deposit(atmScanner.nextDouble());
+                            UsersArrayList.get(i).getChecking().printBalance();
+                            checkingAccountMenu();
+                            break;
+                        }
+                        case 3: {
+                            UsersArrayList.get(i).transferFromChecking();
+                            checkingAccountMenu();
+                            break;
+                        }
+                        case 4: {
+                            UsersArrayList.get(i).getChecking().getTransactionHistory();
+                            checkingAccountMenu();
+                            break;
+                        }
+                        case 5: {
+                            afterLogIntoExistingUserProfile();
+                            break;
+                        }
+                        case 6: {
+                            bootUpATM();
+                            break;
+                        }
+                        case 7: {
+                            UserFunctions.deleteCheckingAccount();
+                            afterLogIntoExistingUserProfile();
 
-                        break;
+                            break;
+                        }
                     }
                 }
+
             }
+        } catch (NullPointerException err) {
+            System.out.println("Please Open an Account First");
+            bootUpATM();
         }
     }
 
@@ -191,59 +187,64 @@ public class Console {
         System.out.println("Please enter your password : ");
         Scanner scanner = new Scanner(System.in);
         String thisPassword = scanner.next();
-        for (int i = 0; i < UsersArrayList.size(); i++) {
-            if (UsersArrayList.get(i).getPassword().equals(thisPassword)) {
-                System.out.println("Welcome to your Savings account: " + "What do you want to do?");
-                System.out.printf("%5s-%5s\n", "[1] ", "Withdraw");
-                System.out.printf("%5s-%5s\n", "[2] ", "Deposit");
-                System.out.printf("%5s-%5s\n", "[3] ", "Transfer");
-                System.out.printf("%5s-%5s\n", "[4] ", "Print Transaction History");
-                System.out.printf("%5s-%5s\n", "[5] ", "Return to Previous Screen");
-                System.out.printf("%5s-%5s\n", "[6] ", "Return to Main Menu");
-                System.out.printf("%5s-%5s\n", "[7] ", "Close this account");
-                System.out.print(": ");
-                int inputOnSavingsAccountMenu = atmScanner.nextInt();
-                switch (inputOnSavingsAccountMenu) {
-                    case 1: {
-                        System.out.println("Enter amount to withdraw:");
-                        UsersArrayList.get(i).getSavings().withdraw(atmScanner.nextDouble());
-                        UsersArrayList.get(i).getSavings().printBalance();
-                        savingsAccountMenu();
-                        break;
-                    }
-                    case 2: {
-                        System.out.println("Enter amount to deposit:");
-                        UsersArrayList.get(i).getSavings().deposit(atmScanner.nextDouble());
-                        UsersArrayList.get(i).getSavings().printBalance();
-                        savingsAccountMenu();
-                        break;
-                    }
-                    case 3: {
-                        UsersArrayList.get(i).transferFromSavings();
-                        savingsAccountMenu();
-                        break;
-                    }
-                    case 4: {
-                        UsersArrayList.get(i).getSavings().getTransactionHistory();
-                        savingsAccountMenu();
-                        break;
-                    }
-                    case 5: {
-                        afterLogIntoExistingUserProfile();
-                        break;
-                    }
-                    case 6: {
-                        bootUpATM();
-                        break;
-                    }
-                    case 7: {
-                        UserFunctions.deleteSavingsAccount();
-                        afterLogIntoExistingUserProfile();
+        try {
+            for (int i = 0; i < UsersArrayList.size(); i++) {
+                if (UsersArrayList.get(i).getPassword().equals(thisPassword)) {
+                    System.out.println("Welcome to your Savings account: " + "What do you want to do?");
+                    System.out.printf("%5s-%5s\n", "[1] ", "Withdraw");
+                    System.out.printf("%5s-%5s\n", "[2] ", "Deposit");
+                    System.out.printf("%5s-%5s\n", "[3] ", "Transfer");
+                    System.out.printf("%5s-%5s\n", "[4] ", "Print Transaction History");
+                    System.out.printf("%5s-%5s\n", "[5] ", "Return to Previous Screen");
+                    System.out.printf("%5s-%5s\n", "[6] ", "Return to Main Menu");
+                    System.out.printf("%5s-%5s\n", "[7] ", "Close this account");
+                    System.out.print(": ");
+                    int inputOnSavingsAccountMenu = atmScanner.nextInt();
+                    switch (inputOnSavingsAccountMenu) {
+                        case 1: {
+                            System.out.println("Enter amount to withdraw:");
+                            UsersArrayList.get(i).getSavings().withdraw(atmScanner.nextDouble());
+                            UsersArrayList.get(i).getSavings().printBalance();
+                            savingsAccountMenu();
+                            break;
+                        }
+                        case 2: {
+                            System.out.println("Enter amount to deposit:");
+                            UsersArrayList.get(i).getSavings().deposit(atmScanner.nextDouble());
+                            UsersArrayList.get(i).getSavings().printBalance();
+                            savingsAccountMenu();
+                            break;
+                        }
+                        case 3: {
+                            UsersArrayList.get(i).transferFromSavings();
+                            savingsAccountMenu();
+                            break;
+                        }
+                        case 4: {
+                            UsersArrayList.get(i).getSavings().getTransactionHistory();
+                            savingsAccountMenu();
+                            break;
+                        }
+                        case 5: {
+                            afterLogIntoExistingUserProfile();
+                            break;
+                        }
+                        case 6: {
+                            bootUpATM();
+                            break;
+                        }
+                        case 7: {
+                            UserFunctions.deleteSavingsAccount();
+                            afterLogIntoExistingUserProfile();
 
-                        break;
+                            break;
+                        }
                     }
                 }
             }
+        } catch (NullPointerException err) {
+            System.out.println("Please Open an Account First");
+            bootUpATM();
         }
     }
 
@@ -251,59 +252,64 @@ public class Console {
         System.out.println("Please enter your password : ");
         Scanner scanner = new Scanner(System.in);
         String thisPassword = scanner.next();
-        for (int i = 0; i < UsersArrayList.size(); i++) {
-            if (UsersArrayList.get(i).getPassword().equals(thisPassword)) {
-                System.out.println("Welcome to your Investment account: " + "What do you want to do?");
-                System.out.printf("%5s-%5s\n", "[1] ", "Withdraw");
-                System.out.printf("%5s-%5s\n", "[2] ", "Deposit");
-                System.out.printf("%5s-%5s\n", "[3] ", "Transfer");
-                System.out.printf("%5s-%5s\n", "[4] ", "Print Transaction History");
-                System.out.printf("%5s-%5s\n", "[5] ", "Return to Previous Screen");
-                System.out.printf("%5s-%5s\n", "[6] ", "Return to Main Menu");
-                System.out.printf("%5s-%5s\n", "[7] ", "Close this account");
-                System.out.print(": ");
-                int inputOnInvestmentAccountMenu = atmScanner.nextInt();
-                switch (inputOnInvestmentAccountMenu) {
-                    case 1: {
-                        System.out.println("Enter amount to withdraw:");
-                        UsersArrayList.get(i).getInvestment().withdraw(atmScanner.nextDouble());
-                        UsersArrayList.get(i).getInvestment().printBalance();
-                        investmentAccountMenu();
-                        break;
-                    }
-                    case 2: {
-                        System.out.println("Enter amount to deposit:");
-                        UsersArrayList.get(i).getInvestment().deposit(atmScanner.nextDouble());
-                        UsersArrayList.get(i).getInvestment().printBalance();
-                        investmentAccountMenu();
-                        break;
-                    }
-                    case 3: {
-                        UsersArrayList.get(i).transferFromInvestment();
-                        investmentAccountMenu();
-                        break;
-                    }
-                    case 4: {
-                        UsersArrayList.get(i).getInvestment().getTransactionHistory();
-                        investmentAccountMenu();
-                        break;
-                    }
-                    case 5: {
-                        afterLogIntoExistingUserProfile();
-                        break;
-                    }
-                    case 6: {
-                        bootUpATM();
-                        break;
-                    }
-                    case 7: {
-                        UserFunctions.deleteInvestmentAccount();
-                        afterLogIntoExistingUserProfile();
-                        break;
+        try {
+            for (int i = 0; i < UsersArrayList.size(); i++) {
+                if (UsersArrayList.get(i).getPassword().equals(thisPassword)) {
+                    System.out.println("Welcome to your Investment account: " + "What do you want to do?");
+                    System.out.printf("%5s-%5s\n", "[1] ", "Withdraw");
+                    System.out.printf("%5s-%5s\n", "[2] ", "Deposit");
+                    System.out.printf("%5s-%5s\n", "[3] ", "Transfer");
+                    System.out.printf("%5s-%5s\n", "[4] ", "Print Transaction History");
+                    System.out.printf("%5s-%5s\n", "[5] ", "Return to Previous Screen");
+                    System.out.printf("%5s-%5s\n", "[6] ", "Return to Main Menu");
+                    System.out.printf("%5s-%5s\n", "[7] ", "Close this account");
+                    System.out.print(": ");
+                    int inputOnInvestmentAccountMenu = atmScanner.nextInt();
+                    switch (inputOnInvestmentAccountMenu) {
+                        case 1: {
+                            System.out.println("Enter amount to withdraw:");
+                            UsersArrayList.get(i).getInvestment().withdraw(atmScanner.nextDouble());
+                            UsersArrayList.get(i).getInvestment().printBalance();
+                            investmentAccountMenu();
+                            break;
+                        }
+                        case 2: {
+                            System.out.println("Enter amount to deposit:");
+                            UsersArrayList.get(i).getInvestment().deposit(atmScanner.nextDouble());
+                            UsersArrayList.get(i).getInvestment().printBalance();
+                            investmentAccountMenu();
+                            break;
+                        }
+                        case 3: {
+                            UsersArrayList.get(i).transferFromInvestment();
+                            investmentAccountMenu();
+                            break;
+                        }
+                        case 4: {
+                            UsersArrayList.get(i).getInvestment().getTransactionHistory();
+                            investmentAccountMenu();
+                            break;
+                        }
+                        case 5: {
+                            afterLogIntoExistingUserProfile();
+                            break;
+                        }
+                        case 6: {
+                            bootUpATM();
+                            break;
+                        }
+                        case 7: {
+                            UserFunctions.deleteInvestmentAccount();
+                            afterLogIntoExistingUserProfile();
+                            break;
 
+                        }
                     }
                 }
             }
+        } catch (NullPointerException err) {
+            System.out.println("Please Open an Account First");
+            bootUpATM();
         }
     }
 }
